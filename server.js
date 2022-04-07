077a9852e964078399d88152fc87f47f47159237
Joas-Assuncao <joassalvior@gmail.com>
2022-04-07 11:57:56 -0300

    Adicionando server.js configurado para deploy no heroku

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const app = express();
 
+app.use('/', express.static(__dirname, 'build'))
+
 app.listen(process.env.PORT || 3000, (err) => {
     err ? console.log(err) : console.log('Funcionando')
 })
